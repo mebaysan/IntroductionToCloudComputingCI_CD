523e52540df4b7852c78ee9eb79964232de89750
mebaysan <menesbaysan@gmail.com>
2022-02-22 23:57:02 +0300

    old version

--- main.py
+++ main.py
@@ -14,10 +14,5 @@ def greeting(name):
     return f"Hello, {name}!"
 
 
-@app.route("/greeting-new/<name>")
-def greeting_new(name):
-    return f"Hello, {name} from CD by using GCP Cloud Build! "
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
